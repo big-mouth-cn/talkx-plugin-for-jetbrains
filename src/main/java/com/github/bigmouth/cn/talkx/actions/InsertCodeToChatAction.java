47034ed721ccd8b601c9b59951cf41229db44086
huxiao <allen.hu@bianxianmao.com>
2023-07-04 13:26:49 +0800

    0.0.2

---
+++ src/main/java/com/github/bigmouth/cn/talkx/actions/InsertCodeToChatAction.java
@@ -0,0 +1,13 @@
+package com.github.bigmouth.cn.talkx.actions;
+
+/**
+ * @author allen
+ * @date 2023/7/4
+ * @since 1.0.0
+ */
+public class InsertCodeToChatAction extends AbstractCodeAction {
+    @Override
+    protected String getKey() {
+        return "insert-code-to-chat";
+    }
+}
---
+++ src/main/java/com/github/bigmouth/cn/talkx/actions/LanguageTranslationAction.java
@@ -0,0 +1,13 @@
+package com.github.bigmouth.cn.talkx.actions;
+
+/**
+ * @author allen
+ * @date 2023/7/4
+ * @since 1.0.0
+ */
+public class LanguageTranslationAction extends AbstractCodeAction {
+    @Override
+    protected String getKey() {
+        return "language-translation";
+    }
+}
